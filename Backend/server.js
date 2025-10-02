473fd8357665615d2b79d3e32376c513e4f6aed3
Ritika Jaiswal <jaiswalritika2707@gmail.com>
2025-10-02 15:06:15 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const app = require('./app');
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const { initializeSocket } = require('./socket');
+const port = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-}); 
+const server = http.createServer(app);
+
+initializeSocket(server);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
